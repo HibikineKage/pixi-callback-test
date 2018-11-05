8015439719a8e63fc6ce8c757aab6455dad907c4
Hibikine Kage <goodsmile314159@gmail.com>
2018-11-05 18:13:03 +0900

    publish

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import * as PIXI from 'pixi.js';
 import image from './sample.jpg';
 
 const sprite = new PIXI.Sprite(PIXI.Texture.fromImage(image));
-sprite.pointerdown = () => console.log('a');
+// This is not documented function!
+sprite.pointerdown = () => console.log('clicked');
 sprite.interactive = true;
 const app = new PIXI.Application({ width: 256, height: 256 });
 app.stage.addChild(sprite);
